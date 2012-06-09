17a0a663b5180b4ee17df4a7cf96fcf8d871798d
Ricardo Pietrobon <rpietro@duke.edu>
2012-06-09 04:34:31 -0400

    minor changes

--- mturk_obesity.R
+++ mturk_obesity.R
@@ -37,7 +37,18 @@ str(obesityData)
 #names(eras.data)
 attach(obesityData)
 
+#####################################################################################
+#Data management 
+#####################################################################################
+
 summary(obesityData)
+#below will view data in a spreadsheet format
+#View(obesityData)
+#below will list variable names, classes (integer, factor, etc), alternative responses
+str(obesityData)
+#list variable names so that they can be used later
+names(obesityData)
+attach(obesityData)
 
 #total number of missing data points
 countNA(obesityData)
@@ -46,10 +57,17 @@ countNA(obesityData)
 matrixplot(obesityData)
 parcoordMiss(obesityData)
 pbox(obesityData)
+tabmiss(obesityData)
 
 #A matrix with ncol(x)+1 columns, in which each row corresponds to a missing data pattern (1=observed, 0=missing). Rows and columns are sorted in increasing amounts of missing information. The last column and row contain row and column counts, respectively.
 md.pattern(obesityData)
 
+###########################################################################################
+#TABLE 1: DEMOGRAPHICS
+###########################################################################################
+#below are commands to summarize continuous and categorical variables
+summary(What.is.your.age..in.years..)
+CrossTable(What.is.your.gender.)
 
 #######################################################################################
 #template_secondary_data_analysis.R is licensed under a Creative Commons Attribution - Non commercial 3.0 Unported License. You are free: to Share — to copy, distribute and transmit the work to Remix — to adapt the work, under the following conditions: Attribution — You must attribute the work in the manner specified by the author or licensor (but not in any way that suggests that they endorse you or your use of the work). Noncommercial — You may not use this work for commercial purposes. With the understanding that: Waiver — Any of the above conditions can be waived if you get permission from the copyright holder. Public Domain — Where the work or any of its elements is in the public domain under applicable law, that status is in no way affected by the license. Other Rights — In no way are any of the following rights affected by the license: Your fair dealing or fair use rights, or other applicable copyright exceptions and limitations; The author's moral rights; Rights other persons may have either in the work itself or in how the work is used, such as publicity or privacy rights. Notice — For any reuse or distribution, you must make clear to others the license terms of this work. The best way to do this is with a link to this web page. For more details see http://creativecommons.org/licenses/by-nc/3.0/
